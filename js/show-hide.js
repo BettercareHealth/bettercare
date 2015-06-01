ab83ff53f6538959cd742aa35e6850bfcbbf50af
Arthur Attwell <arthur@arthurattwell.com>
2015-06-01 15:29:19 +0200

    Move inline scripts to external js

--- js/show-hide.js
+++ js/show-hide.js
@@ -1,18 +1,22 @@
 /*!
- * Shows and hides elements (if the file isn't running through PrinceXML)
+ * Shows and hides elements. Built from http://www.cssnewbie.com/showhide-content-css-javascript/
  */
 
-if (typeof Prince  === "undefined") {
-	function showHide(shID) {
-	   if (document.getElementById(shID)) {
-		  if (document.getElementById(shID+'-show').style.display != 'none') {
-			 document.getElementById(shID+'-show').style.display = 'none';
-			 document.getElementById(shID).style.display = 'block';
-		  }
-		  else {
-			 document.getElementById(shID+'-show').style.display = 'block';
-			 document.getElementById(shID).style.display = 'none';
-		  }
-	   }
-	}
+function showHide(shID) {
+  if (document.getElementById(shID)) {
+    if (document.getElementById(shID+'-show').style.display != 'none') {
+      document.getElementById(shID+'-show').style.display = 'none';
+      document.getElementById(shID).style.display = 'block';
+    }
+    else {
+      document.getElementById(shID+'-show').style.display = 'block';
+      document.getElementById(shID).style.display = 'none';
+    }
+  }
 }
+
+var s = document.getElementById('help-show');
+s.addEventListener("click", function(){showHide("help")});
+
+var h = document.getElementById('help-hide');
+h.addEventListener("click", function(){showHide("help")});
